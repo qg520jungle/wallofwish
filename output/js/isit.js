fe0b1bc435d797a8d53d13e566e1c3f1d9420764
qg520jungle <bothqia@126.com>
2017-12-26 18:35:48 +0800

    tonight

--- output/js/isit.js
+++ output/js/isit.js
@@ -1,1 +1,1 @@
-$(function(){console.log(1)});
+$(function(){console.log(1);for(var l=["买一套大房子","换一辆好车","开一个自己的店","有一次说走就走的旅行","银行存款突破六位数","父母身体健康","找个对象","世界和平","减肥","早睡早起","每天拍一张照片","坚持锻炼"],n=[1,2,3,3,2,1,1,3,2,2,1,3],o="",s=0;s<l.length;s++){o+='<li class="u-icon-blt b'+n[s]+'"><span>',o+=l[s],o+="</span></li>"}console.log($("#bltbox")),o+='<li class="u-icon-blt b4"><span>',o+='<input type="text" id="user-wish">',o+="</span></li>",$("#bltbox").append(o)});
--- src/js/private/isit.js
+++ src/js/private/isit.js
@@ -1,3 +1,30 @@
 $(function(){
 	console.log(1)
+	var arrList = [
+		'买一套大房子',
+		'换一辆好车',
+		'开一个自己的店',
+		'有一次说走就走的旅行',
+		'银行存款突破六位数',
+		'父母身体健康',
+		'找个对象',
+		'世界和平',
+		'减肥',
+		'早睡早起',
+		'每天拍一张照片',
+		'坚持锻炼'
+	];
+	var arrI = [1,2,3,3,2,1,1,3,2,2,1,3]
+	var str = ''
+	for(var i=0;i<arrList.length;i++){
+		var b = arrI[i]
+		str += '<li class="u-icon-blt b' + b + '"><span>'
+		str += arrList[i]
+		str += '</span></li>'
+	}
+	console.log($('#bltbox'))
+	str += '<li class="u-icon-blt b4"><span>'
+	str += '<input type="text" id="user-wish">'
+	str += '</span></li>'
+	$('#bltbox').append(str)
 })
